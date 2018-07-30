9e5b21c01761f2d30a17ea9bc2a0a7e17b630f74
Christina Williams <ChristinaWilliams@Christinas-iMac.local>
2018-07-29 20:38:51 -0500

    'start files for project'

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// FSJS - Random Quote Generator
+
+// Create the array of quote objects and name it quotes
+
+let quotes [
+
+    { 'blah blah blah': 'by blah blah'},
+    { 'blee blee blee'; 'by blee blee'},
+    { 'blow blow blow': 'by blow blow'},
+    { 'clow clow clow': 'by clow clow'},
+    { 'flow flow flow': 'by flow flow'}
+
+];
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+function getRandomQuote () {
+
+
+};
+
+// Create the printQuote funtion and name it printQuote
+
+function printQuote () {
+
+
+};
+
+// This event listener will respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
